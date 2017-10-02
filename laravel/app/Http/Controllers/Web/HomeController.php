6891d508dbb6d33506eb2e1ab3ddfbf1177e8171
Syehbi Herbian <sebbyveteran80@gmail.com>
2017-10-02 13:02:37 +0700

    pixel

--- laravel/app/Http/Controllers/Web/HomeController.php
+++ laravel/app/Http/Controllers/Web/HomeController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers\Web;
 use App\categories;
 use App\Http\Controllers\Controller;
+use App\invoice;
 use App\lessons;
+use DateTime;
+use Session;
 
 class HomeController extends Controller {
 	//
 	public function index() {
 		# code...
+		$now = new DateTime();
+		$mem_id = Session::get('memberID');
 		$categories = categories::where('enable', '=', 1)->get();
 		$lessons = lessons::where('enable', '=', 1)->get();
-		// $services = services::where('status', '=', 1)->where('members_id', '=', $mem_id)->where('expired', '>=', $now)->first();
-
+		$invoice = invoice::where('status', '=', 1)->where('members_id', '=', $mem_id)->first();
 		return view('web.home', [
 			'categories' => $categories,
 			'lessons' => $lessons,
+			'invoice' => $invoice,
 		]);
 	}
 }
