4043f735e17990a14ae5e9b7823e64fbd458f625
Syehbi Herbian <syehbiherbian@gmail.com>
2017-11-13 11:50:42 +0700

    new

--- laravel/resources/views/contrib/point/reward.blade.php
+++ laravel/resources/views/contrib/point/reward.blade.php
@@ -14,7 +14,7 @@
 <style media="screen">
 .category-sliders .description{
     position: absolute;
-    top: 20%;
+    top: 10%;
     width: 100%;
     color: #fff;
 
@@ -28,30 +28,84 @@
 }
 .profile .description{
     position: absolute;
-    top: 25%;
+    top: 2%;
     width: 100%;
     color: #fff;
     text-align: center;
     left: 0px;
 }
-.profile .description img{
-    filter: invert(100%);
+
+.profile .icon{
+    position: absolute;
+    top: 75%;
+    width: 100%;
+    color: #fff;
+    text-align: center;
+    left: 10%
 }
+/*.profile .description img{
+    filter: invert(100%);
+}*/
 
 .blue{
-    background: #55acee;
+    background: #2ba8e2;
+    color: #fff;
 }
+
 .btn{
     border-radius: 20px!important;
     padding: 5px 20px!important;
     font-size: 10px;
     text-decoration: none;
-    color: #fff;
+    color: #fff !important;
     position: relative;
     display: inline-block;
 }
-
-
+.rounded-circle{
+    padding:0px 0px 0px 0px;
+    padding: 5px;
+    height: 95px;
+    width: 95px !important;
+    -moz-border-radius:47px;
+    -webkit-border-radius:47px;
+    border-radius:47px;
+    margin-left: 10px;
+    margin-right: 10px;
+}
+.rounded-circle p{
+  font-size: 10px;
+  margin: 1px;
+  text-align: center;
+}
+.owl-prev{
+    position: absolute;
+    top: 45%;
+    /* color: transparent; */
+    margin-left: 10px;
+    font-size: 15px;
+    background: #fff;
+    /* padding: 5px; */
+    padding-left: 5px;
+    padding-right: 6px;
+    padding-top: 4px;
+    padding-bottom: 4px;
+    border-radius: 50px;
+}
+.owl-next{
+    position: absolute;
+    top: 45%;
+    /* color: transparent; */
+    margin-right: 10px;
+    font-size: 15px;
+    background: #fff;
+    /* padding: 5px; */
+    padding-left: 5px;
+    padding-right: 6px;
+    padding-top: 4px;
+    padding-bottom: 4px;
+    border-radius: 50px;
+    right: 0px;
+}
 </style>
 @section('content')
 <div class="row">
@@ -94,16 +148,39 @@
                 <h3>Point Saya</h3>
             </div>
             <div class="col-md-12 profile" >
-                <a title="Image 1" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350"></a>
+                <a title="Image 1" href="#"><img class="thumbnail img-responsive" src="{{asset('template/kontributor/background/bg-reward.jpg')}}"></a>
                 <div class="description">
                     <div class="col-md-12">
-                        <img class="photo" src="{{asset('template/kontributor/img/icon/Akun.png')}}" height="110px" width="110px" style="object-fit:scale-down;border-radius: 100%;">
-                        <h4  style="padding-bottom:10px;">{{$contrib->point}} Pts</h4>
+                        <img class="photo" src="{{asset('template/kontributor/img/icon/Akun.png')}}" height="110px" width="110px" style="object-fit:scale-down;border-radius: 100%;filter: invert(100%);">
+                        <h4  style="padding-bottom:0px;">{{$contrib->username}}</h4>
+                        <h4  style="padding-bottom:5px;">{{$contrib->points}} Pts</h4>
 
                     </div>
                 </div>
+                <div class="icon hide">
+                    <div class="col-md-4 rounded-circle" style="background:#1cc327;">
+                            <img class="photo" src="{{asset('template/web/icon/bertanya.png')}}" height="30px" width="30px" style="object-fit:scale-down;">
+                            <p>Bertanya</p>
+                            <p>200 Pts</p>
+                    </div>
+                    <div class="col-md-4 rounded-circle" style="background:#2798cc;">
+                            <img class="photo" src="{{asset('template/web/icon/menjawab.png')}}" height="30px" width="30px" style="object-fit:scale-down;">
+                            <p>Menjawab Pertanyaan</p>
+                            <p>200 Pts</p>
+                    </div>
+                    <div class="col-md-4 rounded-circle" style="background:#a238b9;">
+                            <img class="photo" src="{{asset('template/web/icon/menyelesaikan.png')}}" height="30px" width="30px" style="object-fit:scale-down;">
+                            <p>Menyelesaikan Totorial</p>
+                            <p>200 Pts</p>
+                    </div>
+                </div>
+
             </div>
-            <div class="col-md-12">
+            <div class="col-md-12" style="margin-top:60px;text-align: center;">
+                <a class="btn blue" href="#">Pelajari lebih lanjut tentang point</a>
+                <p style="margin-top:20px; font-size:18px;">Point akan berubah atau bertambah dalam waktu 24 jam</p>
+            </div>
+            <div class="col-md-12" style="margin-top:30px;">
                 <h3>Reward Saya</h3>
             </div>
          <?php foreach ($myreward as $key => $value): ?>
@@ -131,8 +208,8 @@
                     <div class="owl-carousel owl-theme category-sliders" id="category-sliders" style="text-align:center;">
                         <?php foreach ($reward as $key => $value): ?>
                           <?php if ($cat->id == $value->category_id): ?>
-                          <div class="item">
-                             <a title="Image 1" href="#"><img class="thumbnail img-responsive" src="{{$value->image}}"></a>
+                          <div class="item" style="border-radius:10px;height:230px;background:url('{{ $value->image }}');background-position: center;background-repeat: no-repeat;background-size: cover;">
+
                              <div class="description">
                                  <div class="col-md-12">
                                      <h4  style="padding-bottom:10px;">{{$value->name}}</h4>
@@ -156,8 +233,8 @@
     $('.category-sliders').owlCarousel({
     loop:true,
     margin:10,
-    // nav:true,
-    // navText: ["<i class='fa fa-chevron-left' aria-hidden='true'></i>","<i class='fa fa-chevron-right' aria-hidden='true'></i>"],
+    nav:true,
+    navText: ["<i class='fa fa-chevron-left' aria-hidden='true'></i>","<i class='fa fa-chevron-right' aria-hidden='true'></i>"],
     responsive:{
         0:{
             items:1
