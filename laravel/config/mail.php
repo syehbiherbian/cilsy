349946419d751c18175a16418e15f1954f38bfff
Syehbi Herbian <sebbyveteran80@gmail.com>
2017-11-30 10:50:46 +0700

    mail

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -63,8 +63,8 @@ return [
 	*/
 
 	'from' => [
-		'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-		'name' => env('MAIL_FROM_NAME', 'Example'),
+		'address' => env('MAIL_FROM_ADDRESS', 'noreply@clsy.id'),
+		'name' => env('MAIL_FROM_NAME', 'Cilsy Fiolution'),
 	],
 
 	/*
