b524701e6971b4ed591f5ae692706961ffad612c
syehbi herbian <sebbyveteran80@gmail.com>
2018-01-03 13:28:43 +0700

    js

--- laravel/resources/views/web/app.blade.php
+++ laravel/resources/views/web/app.blade.php
@@ -426,8 +426,47 @@
 
       }
     </script>
+    <script>
+      $(document).ready(function() {
+
+  $('.menu-icon').click(function(event){
+    $('#sidebar').toggleClass('sidebar-expand');
+    console.log('clicked');
+
+    event.preventDefault();
+  });
+
+  $('#sidebar ul li').click(function(event) {
+    $('#sidebar ul li').removeClass('icon-active')
+    $(this).addClass('icon-active')
+
+  });
 
 
+  $('.header-menu .has-dropdown').on('click', function(event) {
+    var _this = $(this).children('.dropdown-container');
+
+    if (_this.css('display').toLowerCase() !== 'block') {
+      $('.header-menu .dropdown-container').hide()
+      _this.show()
+    } else {
+      _this.hide()
+    }
+  
+    event.preventDefault();
+  });
+
+  $(document).click(function(){
+    $('.dropdown-container').hide();
+    $('#sidebar').removeClass('sidebar-expand');
+  });
+
+  $('.header-menu .has-dropdown, #sidebar').click(function(e){
+    e.stopPropagation();
+  });
+
+});
+    </script>
     <!-- Search Form Auto complete -->
     <script type="text/javascript">
     $(function() {
