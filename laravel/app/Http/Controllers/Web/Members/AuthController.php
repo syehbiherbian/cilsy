2739bce9edf371b6625d88be531126779d4b009b
syehbi herbian <sebbyveteran80@gmail.com>
2018-01-22 13:49:19 +0700

    bug

--- laravel/app/Http/Controllers/Web/Members/AuthController.php
+++ laravel/app/Http/Controllers/Web/Members/AuthController.php
@@ -304,7 +304,7 @@ class AuthController extends Controller {
 
 	private function forgetMember() {
 		Session::forget('memberID');
-		Session::forget('invoiceCODE')
+		Session::forget('invoiceCODE');
 	}
 
 }
