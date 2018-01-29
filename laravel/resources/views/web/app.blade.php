aeced39926c27c0556671306e824708a3f44b077
syehbi herbian <sebbyveteran80@gmail.com>
2018-01-29 11:06:49 +0700

    apa atuh

--- laravel/resources/views/web/app.blade.php
+++ laravel/resources/views/web/app.blade.php
@@ -227,6 +227,7 @@
     <!-- Navbar -->
 
     <nav class="navbar navbar-default navbar-fixed-top">
+    <?php if (empty(Session::get('memberID'))) {?>
     <div class="global-notification">
       <div class="container">
     <h4>
@@ -238,6 +239,7 @@
 
     </div>  
     </div>
+    <?php } ?>
       <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
