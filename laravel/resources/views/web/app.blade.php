1ecf2e05f0550a5f1904f84b242c1462a81b46d0
syehbi herbian <sebbyveteran80@gmail.com>
2018-03-29 22:25:18 +0700

    hapus countdown

--- laravel/resources/views/web/app.blade.php
+++ laravel/resources/views/web/app.blade.php
@@ -438,7 +438,58 @@ a,
 											<i class="fa fa-user" aria-hidden="true" style="display: inline"></i> &nbsp;Masuk / Daftar
 										</a>
 										 <?php }else{ ?>
-											<a href="{{ url('account/profile') }}">
+											<a href{{ csrf_field() }}
+
+                        <input type="hidden" name="token" value="{{ $token }}">
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Reset Password
+                                </button>
+                            </div>
+                        </div>="{{ url('account/profile') }}">
 												<i class="icon ion-ios-person-outline" aria-hidden="true" style="display: inline"></i>Hi, {{ Auth::guard('members')->user()->username }}
 											</a>
 											<?php } ?>
@@ -736,7 +787,7 @@ a,
     s0.parentNode.insertBefore(s1,s0);
     })();
     </script>
-    <script>
+    {{--  <script>
 // Set the date we're counting down to
     var countDownDate = new Date("Mar 25, 2018 23:59:59").getTime();
 
@@ -764,7 +815,7 @@ a,
             document.getElementById("demo").innerHTML = "EXPIRED";
         }
     }, 1000);
-    </script>
+    </script>  --}}
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
