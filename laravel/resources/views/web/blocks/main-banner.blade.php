6df8392c6787e015bbeeeeb1556ede42c411e22e
syehbi herbian <sebbyveteran80@gmail.com>
2018-04-19 16:49:58 +0700

    sidenav

--- laravel/resources/views/web/blocks/main-banner.blade.php
+++ laravel/resources/views/web/blocks/main-banner.blade.php
@@ -1,16 +1,5 @@
 @push('css')
-<style>
-  .owl-theme .owl-dots .owl-dot span {
-    width: 45px;
-    height: 10px;
-    margin: 5px 7px;
-    background: #D6D6D6;
-    display: block;
-    -webkit-backface-visibility: visible;
-    transition: opacity .2s ease;
-    border-radius: 30px;
-}
-</style>
+
 
 @endpush
 
@@ -36,7 +25,7 @@
     </div>
     <div class="row card-row">
       <div class="card-area">
-        <div class="owl-carousel owl-theme">
+
           <div class="col-md-4">
             <div class="card">
               <strong class="title ">Interaktif</strong>
@@ -59,34 +48,14 @@
               <p class="mt-15">Terdapat ratusan materi berbentuk video di dukung dengan Ebook dan File Praktek</p>
             </div>
           </div>
-        </div>
+
       </div>
     </div>
   </div>
 </section><!-- ./END MAIN BANNER -->
 
 @push('js')
-<script type="text/javascript" src="{{asset('template/web/js/owl.carousel.min.js') }}"></script>
-<script>
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin: 5,
-    // nav:true,
-    responsive:{
-        0:{
-            items:1,
-            dots:false
-        },
-        600:{
-            items:3,
-            dots:false
-        },
-        1000:{
-            items:3
-        }
-    }
-})
-</script>
+
 
 
 @endpush
