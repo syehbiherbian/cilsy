c54aff13abd2b6d17f8d5137ebd0cec865cfa28e
zafranf <zafranfadilah@gmail.com>
2018-04-29 16:12:53 +0700

    change hash to bcrypt

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -13,7 +13,6 @@ use App\Models\Member;
 use App\Models\Package;
 use App\Models\Invoice;
 use App\Models\Service;
-use Hash;
 
 class MembersController extends Controller
 {
@@ -79,7 +78,7 @@ class MembersController extends Controller
           $members->status       = 1;
           $members->username     = $username;
           $members->email        = $email;
-          $members->password     = Hash::make($password);
+          $members->password     = bcrypt($password);
           $members->created_at   = $now;
           $members->save();
             
@@ -503,7 +502,7 @@ class MembersController extends Controller
           $store->status       = 1;
           $store->username     = $username;
           $store->email        = $email;
-          $store->password     = Hash::make($password);
+          $store->password     = bcrypt($password);
           $store->updated_at   = new DateTime();
           $store->save();
           // dd($store);
