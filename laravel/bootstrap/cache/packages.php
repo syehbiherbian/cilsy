9728bc02434892a364876b55ce254150bfe7fcd7
syehbi herbian <sebbyveteran80@gmail.com>
2018-04-30 11:39:19 +0700

    status

---
+++ laravel/bootstrap/cache/packages.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'pbmedia/laravel-ffmpeg' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pbmedia\\LaravelFFMpeg\\FFMpegServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'FFMpeg' => 'Pbmedia\\LaravelFFMpeg\\FFMpegFacade',
+    ),
+  ),
+);
