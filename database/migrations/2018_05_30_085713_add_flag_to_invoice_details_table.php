a0f46be84b559582d3615eec65b3d915444a23a7
Syehbi Herbian <sebbyveteran80@gmail.com>
2018-05-30 08:58:18 +0700

    add flag

--- database/migrations/2018_05_30_085713_add_flag_to_invoice_details_table.php
+++ database/migrations/2018_05_30_085713_add_flag_to_invoice_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFlagToInvoiceTable extends Migration
+class AddFlagToInvoiceDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddFlagToInvoiceTable extends Migration
      */
     public function up()
     {
-        Schema::table('invoice', function (Blueprint $table) {
+        Schema::table('invoice_details', function (Blueprint $table) {
             $table->integer('flag')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddFlagToInvoiceTable extends Migration
      */
     public function down()
     {
-        Schema::table('invoice', function (Blueprint $table) {
+        Schema::table('invoice_details', function (Blueprint $table) {
             //
         });
     }
