eddc92aaa8047ba5829f212312e0d946cdd49922
Syehbi Herbian <sebbyveteran80@gmail.com>
2018-05-31 14:09:34 +0700

    vt web

--- app/Models/TutorialMember.php
+++ app/Models/TutorialMember.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class TutorialMember extends Model
 {
     protected $table = 'tutorial_member';
+    protected $fillable = ['member_id', 'lesson_id', 'flag'];
 }
