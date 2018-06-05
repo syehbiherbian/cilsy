45b8f4f73cd1d9e51819e185e349c9263533ac80
Syehbi Herbian <sebbyveteran80@gmail.com>
2018-06-05 12:52:09 +0700

    fix add harga lesson di invoice detail

--- app/Models/InvoiceDetail.php
+++ app/Models/InvoiceDetail.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class InvoiceDetail extends Model
 {
-    protected $fillable = ['invoice_id', 'lesson_id', 'contributor_id', 'flag'];
+    protected $fillable = ['invoice_id', 'lesson_id', 'contributor_id', 'flag', 'harga_lesson'];
     public $timestamps = true;
 }
