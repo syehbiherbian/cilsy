4635eebe79c491d66a3ee201b18d91d5a1ce220a
fajli <nendoon@gmail.com>
2018-06-05 17:20:39 +0700

    untuk perhitungan contri

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -208,7 +208,7 @@ function income(){
     
       $row = Income::join('lessons', 'lessons.id', '=', 'invoice_details.lesson_id')
       ->where('lessons.contributor_id',$contribID)
-      ->where('flag', '0')->sum('lessons.price');
+      ->where('flag', '0')->sum('invoice_details.harga_lesson');
 
         if(count($row) ==0){
                 $row = Income::join('lessons', 'lessons.id', '=', 'invoice_details.lesson_id')
