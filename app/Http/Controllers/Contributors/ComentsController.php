cf00eea462630f9c6f47c7613cc45d3f551b4ce4
fajli <nendoon@gmail.com>
2018-07-22 19:33:49 +0700

    t

--- app/Http/Controllers/Contributors/ComentsController.php
+++ app/Http/Controllers/Contributors/ComentsController.php
@@ -154,9 +154,9 @@ class ComentsController extends Controller
 
         $lesson = Lesson::Find($lesson_id);
         $contrib = Contributor::find($uid);
-        
+
         $member->notify(new ContribReplyNotification([$member, $lesson, $contrib]));
-        $nimbrung->notify(new ContribReplyNotification([$nimbrung, $lesson, $contrib]));
+        // $nimbrung->notify(new ContribReplyNotification([$nimbrung, $lesson, $contrib]));
 
         $check=DB::table('comments')->where('parent_id',$comment_id)->get();
 
