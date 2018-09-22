e8e9f52c9bbea974dc2ef10b2b56218e2d62b68f
syehbi <sebbyveteran80@gmail.com>
2018-09-22 20:14:05 +0700

    hapus cart

--- app/Http/Controllers/Veritrans/VtwebController.php
+++ app/Http/Controllers/Veritrans/VtwebController.php
@@ -154,7 +154,6 @@ class VtwebController extends Controller {
             // Create New Services
             $this->create_tutorial_member($order_id);
             $this->update_flag($order_id);
-            $this->hapus_cart($order_id);
             // echo "INPUT: " . $input."<br/>";
             // echo "SIGNATURE: " . $signature;
             return response()->json([
@@ -167,7 +166,6 @@ class VtwebController extends Controller {
                 'type' => $type,
                 'notes' => "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type,
             ]);
-            $this->hapus_cart($order_id);
             //send mail invoice pending
             $this->send_mail($order_id);
             return response()->json([
@@ -180,7 +178,6 @@ class VtwebController extends Controller {
                 'type' => $type,
                 'notes' => "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.",
             ]);
-            $this->hapus_cart($order_id);
             return response()->json([
                 'status' => true
             ], 200);
