77003bfd5828219cbb7b6c7d8e9b90863e58fbaa
syehbi <sebbyveteran80@gmail.com>
2018-10-12 17:23:19 +0700

    sukses mail saat bayar dan pending

--- app/Http/Controllers/Web/PaymentController.php
+++ app/Http/Controllers/Web/PaymentController.php
@@ -6,15 +6,25 @@ use Illuminate\Http\Request;
 use Validator;
 use App\Models\Invoice;
 use App\Models\Cart;
+use App\Models\Member;
 use DB;
 use Auth;
 use Session;
+use App\Mail\InvoiceMail;
+use App\Mail\SuksesMail;
 class PaymentController extends Controller
 {
   public function index($response)
   {
     Cart::where('member_id', Auth::guard('members')->user()->id)->delete();
-    
+    $invoice = Invoice::where('code', $order_id)->first();
+    $members = Member::where('id', $invoice->members_id)->first();
+    $send = Member::findOrFail($members->id);
+    if($invoice->status == 2){
+      Mail::to($members->email)->send(new InvoiceMail($send));
+    } else if($invoice->status == 1){
+      Mail::to($members->email)->send(new SuksesMail($send));
+    }
     if($response == 'finish'){
       return view('web.payment.finish');
     }else if($response == 'unfinish'){
